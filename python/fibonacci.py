6d4fadad328075785ab390c933eff8f71ea285a4
Derek Bruckner <derek.bruckner@detroitlabs.com>
2022-03-03 14:15:42 -0500

    Add bottom-up approach to fibonacci.py

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -5,11 +5,12 @@ def fibonacci(index):
         return index
     return fibonacci(index - 1) + fibonacci(index - 2)
 
-# print(fibonacci(5))
-# print(fibonacci(10))
-# print(fibonacci(11))
+print(fibonacci(5))
+print(fibonacci(10))
+print(fibonacci(11))
 
 # Recursive Memoization
+#  - Optimized to reduce number of calculations required
 def fibonacciMemoized(index, cache = None):
     # Check: Initialize cache
     if cache is None: 
@@ -29,3 +30,18 @@ def fibonacciMemoized(index, cache = None):
 print(fibonacciMemoized(5))
 print(fibonacciMemoized(10))
 print(fibonacciMemoized(11))
+
+# Bottom-up 
+# - Optimized to solve sub-problems for when they're needed
+# - Throw away previous calculation to reduce memory (space complexity)
+
+def fibonacciBottomUp(index):
+    a = 1
+    b = 1
+    for i in range(2, index):
+        a, b = b, a + b
+    return b
+
+print(fibonacciBottomUp(5))
+print(fibonacciBottomUp(10))
+print(fibonacciBottomUp(11))
