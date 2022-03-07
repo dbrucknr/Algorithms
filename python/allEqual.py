caf92508140df331964a1434a3884da5491e5ec6
Derek Bruckner <derek.bruckner@detroitlabs.com>
2022-03-07 10:21:55 -0500

    Add allEqual.

---
+++ python/allEqual.py
@@ -0,0 +1,15 @@
+def allEqual(array):
+    for value in array:
+        if value != array[0]:
+            return False
+    return True
+
+# print(allEqual([1, 1, 1]))
+# print(allEqual([1, 3, 1]))
+
+def allEqualComprehension(array):
+    return [array[i] == array[0] for i, v in enumerate(array)]
+
+print(allEqualComprehension([1, 1, 1]))
+print(allEqualComprehension([1, 2, 1]))
+print(allEqualComprehension([2, 3, 2, 1]))
