ca4bb29daff344acdd9ba3e916a5780f6f402439
Derek Bruckner <dbrucknr@umich.edu>
2023-01-12 09:59:02 -0500

    Create difference by example that allows a function to be passed to determine difference criteria.

--- python/helpers/difference.py
+++ python/helpers/difference.py
@@ -1,4 +1,5 @@
-from typing import List
+from math import floor
+from typing import List, Callable
 
 def difference(_array: List, __array: List) -> List:
     """Calculates difference between two arrays"""
@@ -7,4 +8,23 @@ def difference(_array: List, __array: List) -> List:
 
 print(
     difference([1, 2, 3, 3], [1, 2, 4])
+)
+
+def differenceBy(_array: List, __array: List, fn: Callable) -> List:
+    unique = set(map(fn, __array))
+    return [
+        value for value 
+        in list(map(fn, _array)) if value not in unique
+    ]
+
+print(
+    differenceBy(
+        [2.1, 1.2], [2.3, 3.4], floor
+    )
+)
+
+print(
+    differenceBy(
+        [{ "x": 2 }, { "x": 1 }], [{ "x": 1 }], lambda v: v["x"]
+    )
 )
